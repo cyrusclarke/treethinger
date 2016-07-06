2a0db60ef2aafa291f8fc100b926bfe32876d4b1
cyrusclarke <cyrusjclarke@gmail.com>
2016-07-06 19:08:46 +0000

    commit 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treethinger::Application.config.secret_key_base = 'b67dc8ae8f96662fad0997ad547c6df563e142e5bce8f951e7f6fe8165cb383ee7ea2596d047e7e7fc93770b1cf2edc3f11ac698ac936aa3f8fc6be5732c598c'
