ab810a527aaf088aeed4e801ec655f5e85a16e02
cyrusclarke <cyrusjclarke@gmail.com>
2016-07-10 23:02:09 +0000

    setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Treethinger::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options =  { https://treethinger.herokuapp.com/ }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Treethinger::Application.routes.draw do
+  devise_for :users
   root 'dynamic_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
